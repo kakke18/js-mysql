0a18c1227b40354906940acb6e35606e62606731
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-03-20 10:38:56 +0900

    change; directory structure

--- src/js/devserver.js
+++ src/js/devserver.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/bunches', (req, res) =>{
-  const json = require('./bunches.json');
+  const json = require('../../json/bunches.json');
   res.json(json);
 });
 
