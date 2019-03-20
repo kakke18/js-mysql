d207225fb889e3f440970aa5302a32eba6f80a79
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-03-20 14:30:46 +0900

    delete: unnecessary items

--- src/js/devserver.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/bunches', (req, res) =>{
-  const json = require('../../json/bunches.json');
-  res.json(json);
-});
-
-app.listen(3000, () => console.log('Listening on port 3000'))
